419f1134710d0b5ddf60c304ff47e9ed7c7b8cd9
Ben Burk <misprediction@gmail.com>
2016-10-31 23:45:44 -0400

    Added Looksharp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using Looksharp.Champions;
+using Looksharp.Utilities;
+
+namespace Looksharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            try
+            {
+                Base Instance = (Base)Activator.CreateInstance(null, "Looksharp.Champions." + Player.Instance.ChampionName).Unwrap();
+                Chat.Print("LookSharp => " + Player.Instance.ChampionName + " Loaded!");
+            }
+            catch (Exception)
+            {
+                Chat.Print("LookSharp => " + Player.Instance.ChampionName + " Not supported!");
+            }
+            Load.Init();
+        }
+    }
+}
